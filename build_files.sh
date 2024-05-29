85219475ecac25947b949a6940a4833afa5df751
kannadhasanveerasamy <kannancb17@gamil.com>
2024-05-29 12:15:52 +0530

    updated requirements.txt

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,4 @@
 echo "BUILD START"
-
-# create a virtual environment named 'venv' if it doesn't already exist
-python3 -m venv venv
-
-# activate the virtual environment
-source venv/bin/activate
-
 python3 -m pip install -r requirements.txt
 # install all deps in the venv
 pip install -r requirements.txt
@@ -15,4 +8,4 @@ python3 manage.py collectstatic --noinput
 
 echo "BUILD END"
 # [optional] Start the application here 
-# python manage.py runserver
+python manage.py runserver
