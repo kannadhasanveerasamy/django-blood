eab9e7262e4d8224ad0038692b91b38798040c90
kannadhasanveerasamy <kannancb17@gamil.com>
2024-05-29 11:56:04 +0530

    updated Deploy settings

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,18 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 python3 -m pip install -r requirements.txt
+# install all deps in the venv
+pip install -r requirements.txt
 python3.9 manage.py collectstatic
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
+# [optional] Start the application here 
+# python manage.py runserver
